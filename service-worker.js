fba2ad3e93630cb849b712c3ee1e8006162743ac
Mona Lisa <joannagrosshans2@gmail.com>
2021-01-20 13:40:20 -0600

    service workers added

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const APP_PREFIX = "Budget_Tracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./public/css/styles.css",
+    "./routes/api.js"
+];
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeepList.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] );
+                    return caches.delete(keyList[i]);
+                }
+            }))
+        })
+    )
+});
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                // if cache is available, respond with cache
+              console.log('responding with cache : ' + e.request.url)
+              return request
+            } else { 
+                // if there are no cache, try fetching request
+              console.log('file is not cached, fetching : ' + e.request.url)
+              return fetch(e.request)
+          }
+          
+          // You can omit if/else for console.log & put one line below like this too.
+          // return request || fetch(e.request)
+          })
+    )
+  })
