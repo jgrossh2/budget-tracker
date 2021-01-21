9fb57fc5c0b01e3f52317b3239240c0925dff3ba
Mona Lisa <joannagrosshans2@gmail.com>
2021-01-21 14:13:11 -0600

    checking error

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,7 @@ fetch("/api/transaction")
 function populateTotal() {
   // reduce transaction amounts to a single total value
   let total = transactions.reduce((total, t) => {
+    console.log("check=====", transactions)
     return total + parseInt(t.value);
   }, 0);
 
